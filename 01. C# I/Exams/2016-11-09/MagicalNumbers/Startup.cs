06eb24ce73e5986b2960df4c4df2b1329f72abe8
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-11-10 19:34:37 +0200

    Added MagicalNumbers task

---
+++ 01. C# I/Exams/2016-11-09/MagicalNumbers/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MagicalNumbers
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var number = Console.ReadLine();
+
+            var firstDigit = int.Parse(number[0].ToString());
+            var secondDigit = int.Parse(number[1].ToString());
+            var thirdDigit = int.Parse(number[2].ToString());
+
+            double result = 0;
+
+            if (secondDigit % 2 == 0)
+            {
+                result += (firstDigit + secondDigit) * thirdDigit;
+            }
+            else
+            {
+                result += (double)(firstDigit * secondDigit) / thirdDigit;
+            }
+
+            Console.WriteLine("{0:F2}", result);
+        }
+    }
+}
