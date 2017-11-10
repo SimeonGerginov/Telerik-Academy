5df19c61509cc1b324377a3c5b76d8bc202a5070
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-11-10 20:02:56 +0200

    Added GoingToParty task

---
+++ 01. C# I/Exams/2016-11-09/GoingToParty/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace GoingToParty
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var directions = Console.ReadLine();
+            var index = 0;
+            var position = 0;
+
+            while (true)
+            {
+                var symbol = directions[index];
+
+                if (symbol == '^')
+                {
+                    Console.WriteLine("Djor and Djano are at the party at {0}!", index);
+                    break;
+                }
+
+                if (Char.IsLower(symbol))
+                {
+                    position = symbol - 'a' + 1;
+                    index += position;
+
+                    if (index >= directions.Length)
+                    {
+                        Console.WriteLine("Djor and Djano are lost at {0}!", index);
+                        break;
+                    }
+                }
+
+                if(Char.IsUpper(symbol))
+                {
+                    position = symbol - 'A' + 1;
+                    index -= position;
+
+                    if (index < 0)
+                    {
+                        Console.WriteLine("Djor and Djano are lost at {0}!", index);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
