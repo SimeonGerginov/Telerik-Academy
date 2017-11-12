a83c190b62ca5e5a24061115320e8814834af34a
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-11-12 19:37:05 +0200

    Added Busses task

---
+++ 01. C# I/Exams/2016-04-25/Busses/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Busses
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var C = int.Parse(Console.ReadLine());
+            var firstSpeed = int.Parse(Console.ReadLine());
+            var numberOfGroups = 1;
+
+            for (int i = 1; i < C; i++)
+            {
+                var secondSpeed = int.Parse(Console.ReadLine());
+
+                if (firstSpeed == secondSpeed)
+                {
+                    numberOfGroups++;
+                    firstSpeed = secondSpeed;
+                }
+
+                if (firstSpeed > secondSpeed)
+                {
+                    numberOfGroups++;
+                    firstSpeed = secondSpeed;
+                }
+            }
+
+            Console.WriteLine(numberOfGroups);
+        }
+    }
+}
