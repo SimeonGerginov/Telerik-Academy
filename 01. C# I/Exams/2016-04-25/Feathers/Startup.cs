943f363f5998af2eb4b70d967fd10a7ee3b5fd17
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-11-12 19:11:33 +0200

    Added Feathers task

---
+++ 01. C# I/Exams/2016-04-25/Feathers/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Feathers
+{
+    public class Startup
+    {
+        private const long magicNumberForMultiply = 123123123123;
+        private const int magicNumberForDivide = 317;
+
+        public static void Main()
+        {
+            var B = int.Parse(Console.ReadLine());
+            var F = int.Parse(Console.ReadLine());
+
+            double result = (double)F / (double)B;
+
+            if (B % 2 == 0)
+            {
+                result *= (double)magicNumberForMultiply;
+            }
+            else
+            {
+                result /= (double)magicNumberForDivide;
+            }
+
+            Console.WriteLine("{0:F4}", result);
+        }
+    }
+}
