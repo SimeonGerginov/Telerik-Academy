9c509f11e659c483204b2f2c0680a0275a0e00d4
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-11-25 21:52:00 +0200

    Added MythicalNumbers task

---
+++ 01. C# I/Exams/2016-11-10/MythicalNumbers/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MythicalNumbers
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var numberAsString = Console.ReadLine();
+
+            var firstDigit = int.Parse(numberAsString[0].ToString());
+            var secondDigit = int.Parse(numberAsString[1].ToString());
+            var thirdDigit = int.Parse(numberAsString[2].ToString());
+
+            double result = 0;
+
+            if (thirdDigit == 0)
+            {
+                result = firstDigit * secondDigit;
+            }
+            else if (thirdDigit > 0 && thirdDigit <= 5)
+            {
+                result = firstDigit * secondDigit;
+                result = result / thirdDigit;
+            }
+            else // thirdDigit > 5
+            {
+                var sum = firstDigit + secondDigit;
+                result = sum * thirdDigit;
+            }
+
+            Console.WriteLine("{0:F2}", result);
+        }
+    }
+}
