dca4959caa14dfb3e4c2699b25b7ea6d466b095d
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-11-26 14:46:51 +0200

    Added MutantSquirrels task

---
+++ 01. C# I/Exams/2016-04-26/MutantSquirrels/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MutantSquirrels
+{
+    public class Startup
+    {
+        private const double DivideValue = 7;
+        private const double MultiplyValue = 376439;
+
+        public static void Main()
+        {
+            var trees = double.Parse(Console.ReadLine());
+            var branches = double.Parse(Console.ReadLine());
+            var squirrels = double.Parse(Console.ReadLine());
+            var averageNumberOfTails = double.Parse(Console.ReadLine());
+
+            double result = 0;
+
+            double product = trees * branches * squirrels * averageNumberOfTails;
+
+            if (product % 2 != 0)
+            {
+                result = product / DivideValue;
+            }
+            else // product is an even number
+            {
+                result = product * MultiplyValue;
+            }
+
+            Console.WriteLine("{0:F3}", result);
+        }
+    }
+}
