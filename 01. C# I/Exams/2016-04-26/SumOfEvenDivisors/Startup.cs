e4ab6d4ae68f7e22b67bb8296d254fdde652cc33
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-12-02 20:36:34 +0200

    Added SumOfEvenDivisors and SumOfOddDivisors tasks

---
+++ 01. C# I/Exams/2016-04-26/SumOfEvenDivisors/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SumOfEvenDivisors
+{
+    public class Startup
+    {
+        private static int SumOfAllEvenDivisors(int number)
+        {
+            var sum = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if ((number % i == 0) && (i % 2 == 0))
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        public static void Main()
+        {
+            var A = int.Parse(Console.ReadLine());
+            var B = int.Parse(Console.ReadLine());
+
+            var sum = 0;
+
+            for (int i = A; i <= B; i++)
+            {
+                var sumOfDivisors = SumOfAllEvenDivisors(i);
+                sum += sumOfDivisors;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 01. C# I/Exams/2016-04-26/SumOfOddDivisors/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SumOfOddDivisors
+{
+    public class Startup
+    {
+        private static int SumOfAllOddDivisors(int number)
+        {
+            var sum = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if ((number % i == 0) && (i % 2 != 0))
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+
+        public static void Main()
+        {
+            var A = int.Parse(Console.ReadLine());
+            var B = int.Parse(Console.ReadLine());
+
+            var sum = 0;
+
+            for (int i = A; i <= B; i++)
+            {
+                var sumOfDivisors = SumOfAllOddDivisors(i);
+                sum += sumOfDivisors;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
