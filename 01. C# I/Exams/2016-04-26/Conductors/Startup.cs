0f0c707f990ae0e586834da8117f1e2b7d195ff4
SimeonGerginov <srgerginov@uni-sofia.bg>
2017-12-05 14:26:22 +0200

    Added Conductors task

---
+++ 01. C# I/Exams/2016-04-26/Conductors/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Conductors
+{
+    public class Startup
+    {
+        private static string ReverseTicketString(string ticket)
+        {
+            var ticketAsCharArray = ticket.ToCharArray();
+            Array.Reverse(ticketAsCharArray);
+            var outputTicket = new string(ticketAsCharArray);
+
+            return outputTicket;
+        }
+
+        private static int ReplacedTicket(int ticket, int perforator)
+        {
+            var ticketAsString = Convert.ToString(ticket, 2);
+            var perforatorAsString = Convert.ToString(perforator, 2);
+            var zeroString = new string('0', perforatorAsString.Length);
+
+            var outputTicket = ReverseTicketString(ticketAsString);
+
+            var newString = outputTicket.Replace(perforatorAsString, zeroString).ToString();
+            newString = ReverseTicketString(newString);
+
+            return Convert.ToInt32(newString, 2);
+        }
+
+        public static void Main()
+        {
+            var perforator = int.Parse(Console.ReadLine());
+            var numberOfTickets = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfTickets; i++)
+            {
+                var ticket = int.Parse(Console.ReadLine());
+
+                var convertedTicket = ReplacedTicket(ticket, perforator);
+
+                Console.WriteLine(convertedTicket);
+            }
+        }
+    }
+}
