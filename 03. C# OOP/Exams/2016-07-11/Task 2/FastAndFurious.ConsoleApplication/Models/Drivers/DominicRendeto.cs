97487a13d2d5af91eaead066865718a897647def
SimeonGerginov <srgerginov@uni-sofia.bg>
2018-03-12 18:43:18 +0200

    Added all driver classes

---
+++ 03. C# OOP/Exams/2016-07-11/Task 2/FastAndFurious.ConsoleApplication/Models/Drivers/DominicRendeto.cs
@@ -0,0 +1,15 @@
+﻿using FastAndFurious.ConsoleApplication.Models.Drivers.Abstract;
+using FastAndFurious.ConsoleApplication.Common.Enums;
+
+namespace FastAndFurious.ConsoleApplication.Models.Drivers
+{
+    public class DominicRendeto : Driver
+    {
+        private const string DominicRendetoDriverName = "Dominic Rendeto";
+
+        public DominicRendeto() 
+            : base(DominicRendetoDriverName, GenderType.Male)
+        {
+        }
+    }
+}
